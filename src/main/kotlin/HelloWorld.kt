f4d8fde0002671a73de0b855deee68407c5380f3
jie-junyi <3274861360@qq.com>
2022-10-24 00:21:12 +0800

    kotlin

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,8 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    println("Hello,World")
+    println(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()))
+}
